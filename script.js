f841763f3fdb11bd8bb8da20c764e428430760d0
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-16 22:05:09 +0100

    WIP - not workingg

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
 createBtn.addEventListener("click", () => {
+  //Stores the p element as input box
   let inputBox = document.createElement("p");
+  //stores image, too
   let img = document.createElement("img");
   inputBox.className = "input-box";
-  inputBox.setAttribute("contentEditable", "true");
+  inputBox.setAttribute("contenteditable", "true");
   img.src = "images/delete.png";
   notesContainer.appendChild(inputBox).appendChild(img);
+});
+
+notesContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.removeChild();
+  }
 });
