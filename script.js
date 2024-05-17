24f284459d36bde1c1f42321fa5c19b07d7d99a3
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-17 16:14:33 +0100

    Incorrectly had remove child - it's just remove

--- script.js
+++ script.js
@@ -15,6 +15,6 @@ createBtn.addEventListener("click", () => {
 
 notesContainer.addEventListener("click", function (e) {
   if (e.target.tagName === "IMG") {
-    e.target.parentElement.removeChild();
+    e.target.parentElement.remove();
   }
 });
